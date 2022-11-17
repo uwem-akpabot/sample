eb40ae206d79e8de5d27c4ecf96081170cf6980e
Uncle life <uwem@luday.se>
2022-11-17 14:38:26 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <div>
+            &copy; Copyright 2022. All right reserved.
+        </div>
+    )
+}
+export default Footer
